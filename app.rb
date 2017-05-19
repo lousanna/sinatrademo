8013419a37ccde08986748ad4ed19b58ce3b6e15
Lousanna Cai <lousanna@>
2017-05-19 11:31:11 -0400

    Read from cake list

--- app.rb
+++ app.rb
@@ -1,5 +1,15 @@
 require 'sinatra'
+require 'yaml/store'
+
+Choices = Array.new([])
 
 get '/' do
-  "Hello, world"
+    @title="Welcome"
+    f = File.open("cake.list", "r")
+    f.each_line do |line|
+        puts line
+        Choices.push(line)
+    end
+    f.close
+    erb :index
 end
