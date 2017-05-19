3b9ac92288a065491ca12c3f1fef15731b5602aa
Lousanna Cai <lousanna@>
2017-05-19 11:51:43 -0400

    Postgres func added

--- app.rb
+++ app.rb
@@ -1,5 +1,8 @@
 require 'sinatra'
 require 'yaml/store'
+require 'sinatra/activerecord'
+require './config/environments'
+require './models/model'
 
 Choices = Array.new([])
 
@@ -12,4 +15,20 @@ get '/' do
     end
     f.close
     erb :index
+end
+
+post '/submit' do
+    @title = "Database Cake"
+    @model = Model.new(params[:model])
+    if @model.save
+        redirect '/models'
+        else
+        "Sorry, there was an error!"
+    end
+end
+
+get '/models' do
+    @title = "Database Cake"
+    @models = Model.all
+    erb :models
 end
