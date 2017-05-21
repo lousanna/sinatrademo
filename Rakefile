464675df1d8b3a0ce1b7f29f78423dd82dedc0a7
Lousanna Cai <lousanna@>
2017-05-21 00:57:13 -0400

    Added JSON

--- Rakefile
+++ Rakefile
@@ -7,3 +7,9 @@ require 'rake/testtask'
 Rake::TestTask.new do |t|
   t.test_files = FileList['test/*_test.rb']
 end
+
+namespace :db do
+  task :load_config do
+    require "./app"
+  end
+end
--- app.rb
+++ app.rb
@@ -1,11 +1,16 @@
 require 'sinatra'
 require 'yaml/store'
 require 'sinatra/activerecord'
+require 'sinatra/cross_origin'
 require './config/environments'
+require './config/cors'
+require 'json'
+
 
 Choices = Array.new([])
 
 class Model < ActiveRecord::Base
+    validates :cake, presence: true
 end
 
 get '/' do
@@ -23,6 +28,7 @@ post '/submit' do
     @title = "Database Cake"
     @model = Model.new(params[:model])
     if @model.save
+        @model.to_json
         redirect '/models'
         else
         "Sorry, there was an error!"
@@ -34,3 +40,52 @@ get '/models' do
     @models = Model.all
     erb :models
 end
+
+get '/list' do
+    @models = Model.all.to_json
+    erb :json
+end
+
+get '/list/:id' do
+    @models = Model.where(id: params['id']).first.to_json
+    @id = Model.where(id: params['id']).first.id
+    erb :editjson
+end
+
+post '/list' do
+    @model = Model.new(params[:model])
+    
+    if @model.save
+        @model.to_json
+        else
+        halt 422, list.errors.full_messages.to_json
+    end
+end
+
+put '/list/:id' do
+    list = Model.where(id: params['id']).first
+    
+    if list
+        list.cake = params['cake'] if params.has_key?('cake')
+        
+        if list.save
+            list.to_json
+            else
+            halt 422, list.errors.full_messages.to_json
+        end
+    end
+end
+
+delete '/list/:id' do
+
+    list = Model.where(id: params['id'])
+    
+    if list.destroy_all
+        {success: "ok"}.to_json
+        else
+        halt 500
+    end
+    
+    erb :deleted
+end
+
